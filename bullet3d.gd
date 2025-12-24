66fad3c621ad6452f29f466ad339d100342fe299
dawwniel <niellann16@gmail.com>
2025-12-24 13:10:30 -0500

    added bullet emission

---
+++ bullet3d.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@export var bulletSpd = 10.0
+@export var rnge = 40.0
+
+var travelledDis = 0.0
+
+func _physics_process(delta):
+	position += transform.basis.z * bulletSpd * delta
+	travelledDis += bulletSpd * delta
+	if travelledDis > rnge:
+		queue_free()
+	
