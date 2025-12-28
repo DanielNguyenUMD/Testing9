091e58f6e01409c3f700c6269dea2ad1472aa6fc
dawwniel <niellann16@gmail.com>
2025-12-28 13:09:27 -0500

    added mob following behavior (for bat)

--- Scripts/bat_mob.gd
+++ Scripts/bat_mob.gd
@@ -1,6 +1,14 @@
 extends RigidBody3D
 
 @onready var bat_model = %bat_model
+var spd = randf_range(2.0, 10.0)
+
+@onready var player = get_node("/root/game/Player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * spd
 
 func take_damage():
 	bat_model.hurt()
