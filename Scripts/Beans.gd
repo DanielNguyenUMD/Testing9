76ff29728b2fac1184f6d25bc31b4c78cac6fd24
dawwniel <niellann16@gmail.com>
2026-01-08 13:51:14 -0500

    added animations to bean item

--- Scripts/Beans.gd
+++ Scripts/Beans.gd
@@ -1,8 +1,12 @@
 extends RigidBody3D
 
+@onready var anim_beans = %AnimationPlayer
 
 func on_pickup(body):
 	print("Contact with ", body.name)
 	if(body.name == "Player"):
 		print("Picked up beans")
 		queue_free()
+		
+func _ready():
+	anim_beans.play("Idle")
