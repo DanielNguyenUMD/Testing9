a08a6ff72485e0d5b64621de94709ee83d5dbb1b
dawwniel <niellann16@gmail.com>
2026-01-07 22:59:13 -0500

    Added materials/coloring to the Pills item model + backend script logic for the pills in prep for anim and item system

---
+++ ItemScenes/Pills.gd
@@ -0,0 +1,13 @@
+extends RigidBody3D
+
+@onready var anim_pill = %AnimationPlayer
+
+
+func on_pickup(body):
+	print("Contact with ", body.name)
+	if(body.name == "Player"):
+		print("Picked up pills")
+		queue_free()
+		
+func _ready():
+	anim_pill.play("Idle")
